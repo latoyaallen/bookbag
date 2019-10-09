cb01ec38969409a20791676e535187e6a665c0f1
latoyaallen <me@tailorlabs.io>
2019-10-08 20:06:00 -0400

    Add propTypes to Text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Text from './components/Text';
+import thepower from './data/images/thepower.png';
 
 
 const pageTitleStyle = {
   fontSize: '15px',
+}
+
+const image = {
+  flex: 1,
+  height: 500,
+  aspectRatio: 1.5,
+  resizeMode: 'contain',
 }
 
 const booksIveRead = "Books I've Read in 2019";
@@ -14,6 +22,7 @@ function App() {
   return (
     <div>
       <Text text={booksIveRead}/>
+      <img style={image} src={thepower} alt="The Power" />;
       <Text text={bookTitle}/>
       <Text text={bookAuthor}/>
     </div>
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Text = (props) => (
   <div>
     {props.text}
   </div>
 );
+
+Text.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default Text;
